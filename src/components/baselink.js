497339432a42e2d4506b0bde1e05209324113e1b
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2023-09-06 09:41:03 +0530

    changing server base link

--- src/components/baselink.js
+++ src/components/baselink.js
@@ -1,4 +1,5 @@
 // let baseLink = "http://localhost:8025";
-let baseLink = "https://ytdbe.onrender.com";
+// let baseLink = "https://ytdbe.onrender.com";
+let baseLink = "https://puzzled-jay-sunbonnet.cyclic.app";
 
 export default baseLink;
